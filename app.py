4ce0a986ce4f8e5083c69d63326e5d66eb119c6e
andré felipe <ndrfelipebraga@gmail.com>
2024-10-24 21:02:01 -0300

    Primeiro Commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from src.tela_inicial import *
+from src.cliente import *
+
+
+def main():
+    """ Função central que irá executar o código  """
+    menu_inicial()
+    # cadastrar_cliente()
+    # listar_clientes()
+    
+if __name__ == '__main__':
+    main()
---
+++ src/tela_inicial.py
@@ -0,0 +1,13 @@
+def menu_inicial():
+    print(" Seja bem vindo ao sistema de crud.\n")
+    
+    print(" 1 - Sistema Usuário ")
+    print(" 2 - Sistema Estoque ")
+    print(" 3 - Sistema ONG ")
+    print(" 4 - Sair ")
+        
+
+def tela_usuario():
+    while True:
+        menu_inicial()
+        opcao_inicial = int(input("Info"))
